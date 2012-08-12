9eddcee6da093c4fac7602d2f077ed08b5c16fc8
Alexander Kern <alex@kernul.com>
2012-08-11 17:00:54 -0700

    Fix pygments for heroku.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source :rubygems
 gem "sinatra"
 gem "sass", "3.2.0.alpha.261"
 gem "redcarpet"
-gem "pygments.rb"
 gem "builder"
--- lib/kernul/markdown.rb
+++ lib/kernul/markdown.rb
@@ -1,10 +1,12 @@
 require "redcarpet"
-require "pygments"
+require "net/http"
 
 module Kernul
   class PygmentsMarkdownRenderer < Redcarpet::Render::HTML
+    PYGMENTIZE_URL = URI.parse('http://pygmentize.herokuapp.com/')
+
     def block_code(code, language)
-      Pygments.highlight(code, lexer: language)
+      Net::HTTP.post_form(PYGMENTIZE_URL, code: code, lang: language).body
     end
   end
 
