3feb6dd600cca96ebd1ca93c008bfc5715d02aa9
Alex Kern <alex@kern.io>
2024-09-17 23:27:04 -0700

    add prev/next buttons

--- app/p/[...slug]/page.tsx
+++ app/p/[...slug]/page.tsx
@@ -4,6 +4,7 @@ import { allPosts } from "contentlayer/generated";
 import { metadata as layoutMetadata } from "@/app/layout";
 import { Metadata } from "next";
 import { Mdx } from "@/components/mdx-components";
+import Link from "next/link";
 
 interface PostProps {
   params: {
@@ -13,13 +14,25 @@ interface PostProps {
 
 async function getPostFromParams(params: PostProps["params"]) {
   const slug = params?.slug?.join("/");
-  const post = allPosts.find((post) => post.slugAsParams === slug);
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  );
+
+  const postIndex = sortedPosts.findIndex((post) => post.slugAsParams === slug);
+  const post = allPosts[postIndex];
 
   if (!post) {
-    null;
+    return null;
   }
 
-  return post;
+  const nextPost = sortedPosts[postIndex + 1];
+  const prevPost = sortedPosts[postIndex - 1];
+
+  return {
+    ...post,
+    next: nextPost ? nextPost : null,
+    prev: prevPost ? prevPost : null,
+  };
 }
 
 export async function generateMetadata({
@@ -75,6 +88,38 @@ export default async function PostPage({ params }: PostProps) {
       )}
       <hr className="my-4" />
       <Mdx code={post.body.code} />
+      <div className="flex justify-between w-full gap-4 mt-8">
+        {post.prev ? (
+          <Link
+            href={post.prev.slug}
+            className="block no-underline w-[50%] self-stretch"
+          >
+            <div className="h-full rounded bg-white dark:bg-stone-800 border border-stone-200 dark:border-stone-700 shadow-sm hover:shadow-md active:shadow-inset transition-shadow duration-300 px-6 py-4">
+              <p className="text-sm text-stone-600 m-0">&laquo; Newer</p>
+              <h3 className="my-0 text-lg font-semibold leading-tight">
+                {post.prev.title}
+              </h3>
+            </div>
+          </Link>
+        ) : (
+          <div className="w-[50%]" />
+        )}
+        {post.next ? (
+          <Link
+            href={post.next.slug}
+            className="block no-underline w-[50%] self-stretch text-right"
+          >
+            <div className="h-full rounded bg-white dark:bg-stone-800 border border-stone-200 dark:border-stone-700 shadow-sm hover:shadow-md active:shadow-inset transition-shadow duration-300 px-6 py-4">
+              <p className="text-sm text-stone-600 m-0">Older &raquo;</p>
+              <h3 className="my-0 text-lg font-semibold leading-tight">
+                {post.next.title}
+              </h3>
+            </div>
+          </Link>
+        ) : (
+          <div className="w-[50%]" />
+        )}
+      </div>
     </article>
   );
 }
